28dcc90e5e501a5ba5cd8aecfd730e1e0b69cf60
Alina Gavrila <alina.gavrila@softvision.ro>
2018-08-21 16:14:09 +0300

    Method for creating a new employee added - need to be refactored

--- app/src/main/java/com/example/alinagavrila/employees_administration/Company.java
+++ app/src/main/java/com/example/alinagavrila/employees_administration/Company.java
@@ -1,15 +1,143 @@
 package com.example.alinagavrila.employees_administration;
 
+import com.example.alinagavrila.employees_administration.constants.Enums.Platform;
+import com.example.alinagavrila.employees_administration.constants.Enums.ProgrammingLanguage;
 import com.example.alinagavrila.employees_administration.constants.Enums.Gender;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.xml.namespace.NamespaceContext;
 
 public class Company {
     private static List<Employee> employeeList = new ArrayList<>();
 
-    public static void addNewEmployee(Employee employee) {
-        employeeList.add(employee);
+    public static Employee newEmployee() throws IOException {
+        Employee employee = null;
+        Gender gender;
+        BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in));
+        String type = null;
+        System.out.print("New employee type: ");
+        try {
+            type = bufferReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.print("\nName:");
+        String name = bufferReader.readLine();
+        System.out.println("\nCNP:");
+        String CNP = bufferReader.readLine();
+        System.out.println("\nGender:");
+        String gen = bufferReader.readLine();
+        if(gen.equals("F")) {
+            gender = Gender.F;
+        }else {
+            gender = Gender.M;
+        }
+
+        switch(type) {
+            case "Administrative":
+                System.out.print("\nApproves Leave: ");
+                boolean approveLeaves = Boolean.parseBoolean(bufferReader.readLine());
+                System.out.print("\nIs Calculating The Salaries: ");
+                boolean isCalculatingTheSalaries = Boolean.parseBoolean(bufferReader.readLine());
+                employee = new AdministrativeEmployee(name, CNP, gender, type, approveLeaves, isCalculatingTheSalaries);
+                break;
+            case "Dev":
+            case "TlDev":
+                System.out.print("\nProgramming Language: ");
+                ProgrammingLanguage programmingLanguage = null;
+                String progLanguage = bufferReader.readLine();
+                switch (progLanguage) {
+                    case "Java":
+                        programmingLanguage = ProgrammingLanguage.Java;
+                        break;
+                    case "CSharp":
+                        programmingLanguage = ProgrammingLanguage.CSharp;
+                        break;
+                    case "C":
+                        programmingLanguage = ProgrammingLanguage.C;
+                        break;
+                    case "Javascript":
+                        programmingLanguage = ProgrammingLanguage.Javascript;
+                        break;
+                    case "Python":
+                        programmingLanguage = ProgrammingLanguage.Python;
+                        break;
+                    case "DotNet":
+                        programmingLanguage = ProgrammingLanguage.DotNet;
+                        break;
+                    case "Ruby":
+                        programmingLanguage = ProgrammingLanguage.Ruby;
+                        break;
+                    default:
+                        System.out.println("Not a valid programming language");
+                }
+
+                if (type.equals("Dev")) {
+                    employee = new DevEmployee(name, CNP, gender, type, programmingLanguage);
+                } else {
+                    System.out.print("\nNumber Of Team Members: ");
+                    int numberOfTeamMembers = Integer.valueOf(bufferReader.readLine());
+                    employee = new TlDevEmployee(name, CNP, gender, type, programmingLanguage, numberOfTeamMembers);
+                }
+                break;
+            case "Qa":
+            case "TlQa":
+                System.out.print("\nHas ISTQB: ");
+                boolean hasIstqb = Boolean.parseBoolean(bufferReader.readLine());
+                System.out.print("\nIs Manual: ");
+                boolean isManual = Boolean.parseBoolean(bufferReader.readLine());
+                System.out.print("\nPlatfrom");
+                String plat = bufferReader.readLine();
+                Platform platform = null;
+                switch (plat) {
+                    case "web":
+                        platform = Platform.Web;
+                        break;
+                    case "Desktop":
+                        platform = Platform.Desktop;
+                        break;
+                    case "Mobile":
+                        platform = Platform.Mobile;
+                        break;
+                    default:
+                        System.out.println("Not a valid platform");
+                }
+                if (type.equals("Qa")) {
+                    employee = new QaEmployee(name, CNP, gender, type, hasIstqb, platform, isManual);
+                } else {
+                    System.out.print("\nNumber Of Team Members: ");
+                    int numberOfTeamMembers = Integer.valueOf(bufferReader.readLine());
+                    employee = new TlQaEmployee(name, CNP, gender, type, hasIstqb, platform, isManual, numberOfTeamMembers);
+                }
+                break;
+            case "Support":
+                System.out.print("\nInstall Operating System: ");
+                boolean installOperatingSystem = Boolean.parseBoolean(bufferReader.readLine());
+                System.out.print("\nNetwork Support: ");
+                boolean networkSupport = Boolean.parseBoolean(bufferReader.readLine());
+                System.out.print("\nHardware Support: ");
+                boolean hardwareSupport = Boolean.parseBoolean(bufferReader.readLine());
+                employee = new SupportEmployee(name, CNP, gender, type, installOperatingSystem, networkSupport, hardwareSupport);
+                break;
+            case "Pm":
+                System.out.print("\nNumber Of employees: ");
+                int numberOfEmployees = Integer.valueOf(bufferReader.readLine());
+                employee = new PmEmployee(name, CNP, gender, type, numberOfEmployees);
+                break;
+            default:
+                System.out.println("Not a valid type of employee");
+        }
+        return employee;
+    }
+
+    public static void addNewEmployee(Employee employee) throws IOException {
+        employeeList.add(newEmployee());
     }
 
     public static void removeEmployee(Employee employee) {
@@ -46,7 +174,7 @@ public class Company {
         }
     }
 
-    public static void consoleMenu(Employee employee) {
+    public static void consoleMenu(Employee employee) throws IOException {
         System.out.println("***** Company Menu *****\n\n");
         System.out.println("1. Add New Employee \n");
         System.out.println("2. Update Employee information \n");
@@ -55,18 +183,24 @@ public class Company {
         System.out.println("5. View Employee List \n");
         System.out.println("6. Exit \n\n");
         System.out.println("Choose an action from menu:");
-        String menuOption = System.console().readLine();
+        BufferedReader bufferReader = new BufferedReader(new InputStreamReader(System.in));
+        String menuOption = null;
+        try {
+            menuOption = bufferReader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        while(menuOption != "6") {
+        while(!menuOption.equals("6")) {
             switch (menuOption) {
                 case "1":
-                    addNewEmployee(employee);
+                    addNewEmployee(newEmployee());
                     break;
                 case "2":
                     System.out.println("Insert key to be modified: ");
-                    String key = System.console().readLine();
+                    String key = bufferReader.readLine();
                     System.out.println("Insert new value for the key: ");
-                    String value = System.console().readLine();
+                    String value = bufferReader.readLine();
                     updateEmployee(employee, key, value);
                     break;
                 case "3":
@@ -80,13 +214,13 @@ public class Company {
                     break;
                 default:
                     System.out.println("Option not valid");
-                    break;
             }
-            menuOption = System.console().readLine();
+            System.out.println("Choose an action from menu:");
+            menuOption = bufferReader.readLine();
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Employee employee = new AdministrativeEmployee("Popescu Mihai", "1870315151541", Gender.M, "Administrative", true, true);
         consoleMenu(employee);
     }
--- app/src/main/java/com/example/alinagavrila/employees_administration/constants/Enums.java
+++ app/src/main/java/com/example/alinagavrila/employees_administration/constants/Enums.java
@@ -3,6 +3,6 @@ package com.example.alinagavrila.employees_administration.constants;
 public class Enums {
     public enum Gender { F, M };
     public enum EmployeeType {Administrative ,Dev , Pm, Qa, Support,  TlDev, TlQa};
-    public enum ProgrammingLanguage {Java, CSharp, CPlusPlus, C, Python, Javascript, DotNet, Ruby};
+    public enum ProgrammingLanguage {Java, CSharp, C, Python, Javascript, DotNet, Ruby};
     public enum Platform {Mobile, Web, Desktop};
 }
